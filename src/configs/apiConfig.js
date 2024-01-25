4951ed2ae5bfa7d99fb44e0e873e8ab7693d8953
smijithc <smijith@abacies.com>
2024-01-25 18:18:02 +0530

    updates in the api urls

--- src/configs/apiConfig.js
+++ src/configs/apiConfig.js
@@ -3,7 +3,7 @@
 //Api config options
 const apiConfig = {
     api: {
-       url: 'https://qa-campaign-middleware-b9d42fc3bd5d.herokuapp.com/v1/'
+       url: 'https://qa-campaign-middleware-staging-d69b75b2f51b.herokuapp.com/v1/' //https://qa-campaign-middleware-b9d42fc3bd5d.herokuapp.com/v1/
     },
     token: {
       url2: ''
